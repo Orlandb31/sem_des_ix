99749e44afb1ff4d368f99e62161ac10d8faac50
Jesus Martinez <jesmartinez@bgeneral.com>
2022-07-19 01:26:36 -0500

    confirm page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { EventInfoComponent } from './pages/event-info/event-info.component';
+import { BuyRequestComponent } from './pages/buy-request/buy-request.component';
+import { BuyConfirmComponent } from './pages/buy-confirm/buy-confirm.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: "hola",
     component: LandingComponent
   },
   {
-    path: "",
+    path: "holis",
     component: EventInfoComponent
-  }  
+  },
+  {
+    path: "holis2",
+    component: BuyRequestComponent
+  },
+  {
+    path: "",
+    component: BuyConfirmComponent
+  } 
 ];
 
 @NgModule({
---
+++ frontend/src/app/pages/buy-request/buy-request.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuyRequestComponent } from './buy-request.component';
+
+describe('BuyRequestComponent', () => {
+  let component: BuyRequestComponent;
+  let fixture: ComponentFixture<BuyRequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuyRequestComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BuyRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
